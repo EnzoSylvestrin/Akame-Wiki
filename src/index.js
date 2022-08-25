b2b5042363e1c97f51a068fe597026a3bc2c49c5
EnzoSylvestrin <enzospavani@gmail.com>
2022-08-25 20:00:20 -0300

    Alterações na rota

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import Home from './pages/Home';
 import About from './pages/about/About';
 import { Routes, Route } from 'react-router';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter Basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/Akame-Wiki" element={<Home />} />
         <Route path="/search" element={<About />} />
       </Routes>
     </BrowserRouter>
